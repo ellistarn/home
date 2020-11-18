607596afdc81a48a199fc123aaae398e1c7a1cab
Ellis Tarn <ellistarn@gmail.com>
2020-11-18 12:01:02 -0800

    Moved to https from ssh for install-home.sh

--- bin/install-home.sh
+++ bin/install-home.sh
@@ -2,6 +2,6 @@
 
 cd ~
 git init
-git remote add origin git@github.com:ellistarn/home.git
+git remote add origin https://github.com/ellistarn/home.git
 git fetch origin
 git reset --hard origin/main
